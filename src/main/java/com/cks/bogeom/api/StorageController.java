8b005fe289e6554d0d90ac0abeabdbd7fbcdd4a7
eunsung-choi <dmstjd2910000@naver.com>
2023-05-29 14:47:11 +0900

    fix: ReviewController

--- src/main/java/com/cks/bogeom/api/StorageController.java
+++ src/main/java/com/cks/bogeom/api/StorageController.java
@@ -1,13 +1,10 @@
 package com.cks.bogeom.api;
 
-import com.cks.bogeom.domain.ImageData;
-import com.cks.bogeom.service.StorageService;
 import com.cks.bogeom.utils.ImageUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,8 +17,6 @@ import java.io.IOException;
 public class StorageController {
 
     RestTemplate restTemplate = new RestTemplate();
-
-    final private StorageService storageService;
 
     // Upload image
     @PostMapping("/image")
@@ -46,9 +41,9 @@ public class StorageController {
     @GetMapping("image/{fileName}")
     public ResponseEntity<?> downloadImage(@PathVariable("fileName") String fileName) {
 
-        byte[] downloadImage = storageService.downloadImage(fileName);
+//        byte[] downloadImage = storageService.downloadImage(fileName);
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.IMAGE_PNG)
-                .body(downloadImage);
+                .body("downloadImage");
     }
 }
